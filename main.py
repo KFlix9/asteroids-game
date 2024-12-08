befb2246aa63f87a0279e2d23ce16ff7b9b33cc9
Felix Krueger <96026278+KFlix9@users.noreply.github.com>
2024-12-08 20:19:47 +0100

    modified: main, adding game loop

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import pygame 
+import pygame as pg
+from pygame.locals import *
 from constants import *
 
 
 def main():
+    pg.init()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        screen.fill('#BF616A')
+        # screen.fill('#BF616A') you can give this any color code
+        pg.display.flip()
 
 
 if __name__ == "__main__":
