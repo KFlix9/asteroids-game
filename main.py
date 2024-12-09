4278eced0e85052d1406639c7c3c2f0dfd2effb0
Felix Krueger <96026278+KFlix9@users.noreply.github.com>
2024-12-09 22:14:00 +0100

    added: circleshape class, player class

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from constants import *
 
 def main():
     pg.init()
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pg.time.Clock()
+    dt = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-        screen.fill('#BF616A')
-        # screen.fill('#BF616A') you can give this any color code
+
+        screen.fill('black')
         pg.display.flip()
+
+        # limit FPS rate
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
