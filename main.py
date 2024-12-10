83aa728a14c10100661fb03ded94dcd4b21f10ac
Felix Krueger <96026278+KFlix9@users.noreply.github.com>
2024-12-10 20:21:25 +0100

    moved: .gitignore (this slipped in the cache) / modified: player can rotate

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
                 return
 
         screen.fill('black')
+        player.update(dt)    #turn
         player.draw(screen)
         pg.display.flip()
 
--- player.py
+++ player.py
@@ -1,17 +1,16 @@
 import pygame as pg
 
-from constants import PLAYER_RADIUS 
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-
-    rotation = 0
+        self.rotation = 0
 
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        forward = pg.Vector2(0, 1).rotate(self.rotation)
+        right = pg.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
@@ -19,3 +18,15 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pg.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        print(f'{self.rotation}')
+
+    def update(self, dt):
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_q]:
+            self.rotate(dt * -1)
+        if keys[pg.K_e]:
+            self.rotate(dt) 
